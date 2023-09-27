5cc103d9a8a52204b5d3bf915473584ec1cd20ce
CyndiCodes <huertascyn@gmail.com>
2023-09-27 18:06:51 -0400

    cards layed out nicer in work tab

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,49 +1,138 @@
-import projectOne from '../assets/capture.jpg'
+import projectOne from '../assets/posterCreation.jpg'
+import projectTwo from '../assets/travelTracker.jpg'
+import weatherApp from '../assets/weatherApp.jpg'
+
 
 export default function Work() {
     return (
-        <div className="row">
-        My Work
-        <div className="col">
-            <a href="#">
-          <div className="card" style={{width: "18rem"}}>
-          <img src={projectOne} className="card-img-top" alt="..."></img>
-            <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    
-            </div>
+      <>
+      <div className="row"> My Work
+
+      </div>
+      <div className="row">
+     
+      <div className="col">
+          <a href="#">
+        <div className="card" style={{width: "18rem"}}>
+        <img src={projectTwo} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Virtual push pin map to track your travel on the go!</p>
+  
           </div>
-            </a>
         </div>
+          </a>
+      </div>
+      
+      <div className="col">
+          <a href="#">
+        <div className="card" style={{width: "18rem"}}>
+        <img src={projectOne} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Create an inspirational poster!</p>
+  
+          </div>
+        </div>
+          </a>
+      </div>
         
-        <div className="col">
-            <a href="#">
-          <div className="card" style={{width: "18rem"}}>
-          <img src={projectOne} className="card-img-top" alt="..."></img>
-            <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    
-            </div>
+      <div className="col">
+          <a href="#" >
+        <div className="card" style={{width: "18rem"}}>
+        <img src={weatherApp} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Get the weather app</p>
+  
           </div>
-            </a>
         </div>
-          
-        <div className="col">
-            <a href="#" >
-          <div className="card" style={{width: "18rem"}}>
-          <img src={projectOne} className="card-img-top" alt="..."></img>
-            <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    
-            </div>
+          </a>
+      </div>
+     
+      <div className="col">
+          <a href="#" >
+        <div className="card" style={{width: "18rem"}}>
+        <img src={weatherApp} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Get the weather app</p>
+  
           </div>
-            </a>
         </div>
-       
+          </a>
+      </div>
+    
+
+      </div>
+      <div className="row">
+   
+      <div className="col">
+          <a href="#">
+        <div className="card" style={{width: "18rem"}}>
+        <img src={projectTwo} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Virtual push pin map to track your travel on the go!</p>
+  
+          </div>
         </div>
+          </a>
+      </div>
+      
+      <div className="col">
+          <a href="#">
+        <div className="card" style={{width: "18rem"}}>
+        <img src={projectOne} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Create an inspirational poster!</p>
+  
+          </div>
+        </div>
+          </a>
+      </div>
+        
+      <div className="col">
+          <a href="#" >
+        <div className="card" style={{width: "18rem"}}>
+        <img src={weatherApp} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Get the weather app</p>
+  
+          </div>
+        </div>
+          </a>
+      </div>
+     
+      <div className="col">
+          <a href="#" >
+        <div className="card" style={{width: "18rem"}}>
+        <img src={weatherApp} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Get the weather app</p>
+  
+          </div>
+        </div>
+          </a>
+      </div>
+    
+
+      </div>
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      </>
+
+        
     );
   }
   
