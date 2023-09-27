2740a800e7bf982c2664dae3e2f0ac7a2d81862c
CyndiCodes <huertascyn@gmail.com>
2023-09-27 18:25:36 -0400

    layout made for contact me form, form only, currently not working

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,26 @@
 export default function Contact() {
     return (
-        <div className="row">
-        Contact Me
-        <div className="col">Email</div>
-        <div className="col">Phone</div>
-        <div className="col">Github</div>
+        <>
+        <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Name</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+          <div id="emailHelp" class="form-text"></div>
         </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+          <div id="emailHelp" class="form-text">Your email will never be shared with anyone else.</div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputMessage1" class="form-label">Message</label>
+          <input type="Message" class="form-control" id="exampleInputMessage1"></input>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+        
+        
+        </>
     );
   }
