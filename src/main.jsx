e8e2f79fbc75fdf1af368d49e030ec4e6743ffe7
CyndiCodes <huertascyn@gmail.com>
2023-09-27 20:34:26 -0400

    added index and element to main.jsx to make the about page the first page yo usee when you visit the app

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,10 @@ const router =createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
+        index: true,
+        element: <About/>
+      },
+      {
         path: '/About',
         element: <About />,
       },
