bfa54984016650563b0dcf09a85dc1817d74a6f1
CyndiCodes <huertascyn@gmail.com>
2023-09-27 20:28:21 -0400

    updated logo import in footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 import gitHubLogo from '../assets/gitHubLogo.jpg'
-import linkedInLogo from '../assets/LinkedInLogo.jpg'
+import linkedInLogo from '../assets/linkedInLogo.jpg'
 
 function Footer() {
     return (
