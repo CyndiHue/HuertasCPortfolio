e12e9dbe2d01ec165bf203c6a7d02cf4a7e321d1
CyndiCodes <huertascyn@gmail.com>
2023-10-17 17:49:16 -0400

    cards now the same size, margins added to logo in footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
         
         <div className="row">About Me</div>
         <img src={profilePic} className="row profile rounded-5" alt="" style={{width: "200px"}}></img>
-        <div className="row">Welcome! I am a Full Stack Web Developer looking to showcase my most recent work. If you click the Work link above you will find links to a few projects of mine. You may also contact me directly via email by clicking Contact Me tab. Thank you for checking out my work and I hope you enjoy!</div>
+        <div className="row mb-4">Welcome! I am a Full Stack Web Developer looking to showcase my most recent work. If you click the Work link above you will find links to a few projects of mine. You may also contact me directly via email by clicking Contact Me tab. Thank you for checking out my work and I hope you enjoy!</div>
         
         </>
 
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -10,126 +10,109 @@ export default function Work() {
       <div className="row"> My Work
 
       </div>
-      <div className="row">
-     
-      <div className="col">
+      <div className="container">
+      <div className="row mb-4">
+        <div className="col">
           <a href="https://travel-tracker-msc-0fe902780013.herokuapp.com/">
-        <div className="card" style={{width: "18rem"}}>
-        <img src={projectTwo} className="card-img-top" alt="..."></img>
-          <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Virtual push pin map to track your travel on the go!</p>
-  
-          </div>
-        </div>
+            <div className="card h-100">
+              <img src={projectTwo} className="card-img-top" alt="Travel Tracker" />
+              <div className="card-body">
+                <h5 className="card-title">Travel Tracker</h5>
+                <p className="card-text">Virtual push pin map to track your travel on the go!</p>
+              </div>
+            </div>
           </a>
-      </div>
-      
-      <div className="col">
+        </div>
+
+        <div className="col">
           <a href="https://cdziedzic.github.io/Nebula-Narratives/">
-        <div className="card" style={{width: "18rem"}}>
-        <img src={projectOne} className="card-img-top" alt="..."></img>
-          <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Create an inspirational poster!</p>
-  
-          </div>
-        </div>
+            <div className="card h-100">
+              <img src={projectOne} className="card-img-top" alt="Poster Creation" />
+              <div className="card-body">
+                <h5 className="card-title">Poster Creation</h5>
+                <p className="card-text">Create an inspirational poster!</p>
+              </div>
+            </div>
           </a>
-      </div>
-        
-      <div className="col">
-          <a href="https://cyndicodes.github.io/getTheWeather/" >
-        <div className="card" style={{width: "18rem"}}>
-        <img src={weatherApp} className="card-img-top" alt="..."></img>
-          <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Get the weather app</p>
-  
-          </div>
         </div>
-          </a>
-      </div>
-     
-      <div className="col">
-          <a href="https://project-3-app-mck-b96b86dfcabe.herokuapp.com/" >
-        <div className="card" style={{width: "18rem"}}>
-        <img src={projectThree} className="card-img-top" alt="..."></img>
-          <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Get the weather app</p>
-  
-          </div>
-        </div>
-          </a>
-      </div>
-    
 
-      </div>
-      <div className="row">
-   
-      <div className="col">
-          <a href="#">
-        <div className="card" style={{width: "18rem"}}>
-        <img src={projectTwo} className="card-img-top" alt="..."></img>
-          <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Virtual push pin map to track your travel on the go!</p>
-  
-          </div>
-        </div>
+        <div className="col">
+          <a href="https://cyndicodes.github.io/getTheWeather/">
+            <div className="card h-100">
+              <img src={weatherApp} className="card-img-top" alt="Weather App" />
+              <div className="card-body">
+                <h5 className="card-title">Weather App</h5>
+                <p className="card-text">Get the weather app</p>
+              </div>
+            </div>
           </a>
-      </div>
-      
-      <div className="col">
-          <a href="#">
-        <div className="card" style={{width: "18rem"}}>
-        <img src={projectOne} className="card-img-top" alt="..."></img>
-          <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Create an inspirational poster!</p>
-  
-          </div>
         </div>
-          </a>
-      </div>
-        
-      <div className="col">
-          <a href="#" >
-        <div className="card" style={{width: "18rem"}}>
-        <img src={weatherApp} className="card-img-top" alt="..."></img>
-          <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Get the weather app</p>
-  
-          </div>
-        </div>
-          </a>
-      </div>
-     
-      <div className="col">
-          <a href="#" >
-        <div className="card" style={{width: "18rem"}}>
-        <img src={weatherApp} className="card-img-top" alt="..."></img>
-          <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Get the weather app</p>
-  
-          </div>
-        </div>
-          </a>
-      </div>
-    
 
+        <div className="col">
+          <a href="https://project-3-app-mck-b96b86dfcabe.herokuapp.com/">
+            <div className="card h-100">
+              <img src={projectThree} className="card-img-top" alt="Project Three" />
+              <div className="card-body">
+                <h5 className="card-title">Project Three</h5>
+                <p className="card-text">Description for Project Three</p>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
-      
-      
-      
-      
-      
-      
-      
-      
+    </div>
+
+    <div className="container">
+      <div className="row mb-4">
+        <div className="col">
+          <a href="https://travel-tracker-msc-0fe902780013.herokuapp.com/">
+            <div className="card h-100">
+              <img src={projectTwo} className="card-img-top" alt="Travel Tracker" />
+              <div className="card-body">
+                <h5 className="card-title">Travel Tracker</h5>
+                <p className="card-text">Virtual push pin map to track your travel on the go!</p>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="col">
+          <a href="https://cdziedzic.github.io/Nebula-Narratives/">
+            <div className="card h-100">
+              <img src={projectOne} className="card-img-top" alt="Poster Creation" />
+              <div className="card-body">
+                <h5 className="card-title">Poster Creation</h5>
+                <p className="card-text">Create an inspirational poster!</p>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="col">
+          <a href="https://cyndicodes.github.io/getTheWeather/">
+            <div className="card h-100">
+              <img src={weatherApp} className="card-img-top" alt="Weather App" />
+              <div className="card-body">
+                <h5 className="card-title">Weather App</h5>
+                <p className="card-text">Get the weather app</p>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="col">
+          <a href="https://project-3-app-mck-b96b86dfcabe.herokuapp.com/">
+            <div className="card h-100">
+              <img src={projectThree} className="card-img-top" alt="Project Three" />
+              <div className="card-body">
+                <h5 className="card-title">Project Three</h5>
+                <p className="card-text">Description for Project Three</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
       
       </>
 
