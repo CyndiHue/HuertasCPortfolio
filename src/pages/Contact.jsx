7569e79d9d1d9c54f839d9bb85ce50450554bc2a
CyndiCodes <huertascyn@gmail.com>
2023-10-18 18:37:49 -0400

    email submit added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,26 +1,44 @@
-export default function Contact() {
-    return (
-        <>
-        <form>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">Name</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-          <div id="emailHelp" className="form-text"></div>
-        </div>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-          <div id="emailHelp" className="form-text">Your email will never be shared with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label for="exampleInputMessage1" className="form-label">Message</label>
-          <input type="Message" className="form-control" id="exampleInputMessage1"></input>
-        </div>
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-        
-        
-        </>
-    );
+function ContactForm() {
+  const [state, handleSubmit] = useForm("mrgwrwon");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
   }
+  return (
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
+}
+function App() {
+  return (
+    <ContactForm />
+  );
+}
+export default App;
