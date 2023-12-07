85278e8617d81eb8c1367730cacdbcd412357a59
CyndiCodes <huertascyn@gmail.com>
2023-12-07 12:00:41 -0500

    navbar component updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,42 @@
 import { Link, useLocation } from 'react-router-dom';
+import gitHubLogo from '../assets/gitHubLogo.jpg'
+import linkedInLogo from '../assets/linkedInLogo.jpg' 
+import '../../index.html';
+import '../App.css';
 
 
 function Navbar() {
   const currentPage = useLocation().pathname;
+  const glowTextStyle = {
+    color: 'white',
+    textShadow: '0 0 10px #AFB3F7',
+  };
+
+
+
+  const linkStyle = {
+    color: 'white',
+    textDecoration: 'none', 
+  };
+
+  const activeLinkStyle = {
+    color: 'white',
+    borderBottom: '2px solid white',
+  };
+
+  const hoverLinkStyle = {
+    color: 'red' 
+  };
 
   return (
-    
-    <ul className="nav navbar ">
-    
-    <h1>Cynthia Huertas</h1>
-    
+    <>
+    <ul className="nav navbar">
+      <h1 className="navbarCustomText glow-text">Cynthia Huertas</h1>
       <li className="nav-item ms-auto">
         <Link
           to="/About"
-          className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === '/About' ? 'nav-link active' : 'nav-'}
+          style={currentPage === '/About' ? activeLinkStyle : linkStyle}
         >
           About
         </Link>
@@ -22,6 +45,7 @@ function Navbar() {
         <Link
           to="/Work"
           className={currentPage === '/Work' ? 'nav-link active' : 'nav-link'}
+          style={currentPage === '/Work' ? activeLinkStyle : Style}
         >
           Work
         </Link>
@@ -30,6 +54,7 @@ function Navbar() {
         <Link
           to="/Contact"
           className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+          style={currentPage === '/Contact' ? activeLinkStyle : linkStyle}
         >
           Contact Me
         </Link>
@@ -38,12 +63,26 @@ function Navbar() {
         <Link
           to="/Resume"
           className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+          style={currentPage === '/Resume' ? activeLinkStyle : linkStyle}
         >
           Resume
         </Link>
       </li>
+      
     </ul>
+    <nav className="nav navbar ">
+  <div className="container  d-flex justify-content-end">
+    <a className="navbar-brand" href="https://github.com/CyndiHue">
+      <img src={gitHubLogo} alt="" width="30" height="24"></img>
+    </a>
+   
+    <a className="navbar-brand mx-4" href="https://www.linkedin.com/in/cynthia-huertas-090498288/">
+      <img src={linkedInLogo} alt="" width="30" height="24"></img>
+    </a>
+  
+  </div>
+</nav>
+    </>
   );
 }
-
 export default Navbar;
