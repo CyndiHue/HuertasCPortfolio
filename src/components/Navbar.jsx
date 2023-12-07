30c4bb2b31c86a37bc1aea29ff90af05b0284336
CyndiCodes <huertascyn@gmail.com>
2023-12-07 13:53:32 -0500

    navbar style adjustments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,18 +59,19 @@ function Navbar() {
         </Link>
       </li>
     </ul>
-    <nav className="row navbar bg-body-tertiary">
-  <div className="col container  d-flex justify-content-center">
-    <a className="navbar-brand" href="https://github.com/CyndiCodes">
-      <img src={gitHubLogo} alt="" width="30" height="24"></img>
-    </a>
-    <div className="col container  d-flex justify-content-center">
-    <a className="navbar-brand" href="https://www.linkedin.com/in/cyndi-codes-090498288/">
-      <img src={linkedInLogo} alt="" width="30" height="24"></img>
-    </a>
-  </div>
-  </div>
-</nav>
+  
+    <nav className="nav navbar ">
+      <div className="container  d-flex justify-content-end">
+        <a className="navbar-brand" href="https://github.com/CyndiHue">
+          <img src={gitHubLogo} alt="" width="30" height="24"></img>
+        </a>
+      
+        <a className="navbar-brand mx-4" href="https://www.linkedin.com/in/cynthia-huertas-090498288/">
+          <img src={linkedInLogo} alt="" width="30" height="24"></img>
+        </a>
+      
+      </div>
+    </nav>
    </>
   );
 }
